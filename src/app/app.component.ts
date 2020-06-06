917549d389eabdd0167b46ad1535bff200836a1a
Mnemba Chambuya <karimnembac@gmail.com>
2020-06-06 15:17:20 +0300

    for test deployment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'company-site';
+
+  constructor(private renderer: Renderer2) { }
+
+  changeAproachTxt(el: ElementRef) {
+    console.log(el)
+  }
 }
