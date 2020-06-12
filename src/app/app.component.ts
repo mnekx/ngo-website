546ee6119a57b3af08836316f7562e143eb7d58c
Mnemba Chambuya <karimnembac@gmail.com>
2020-06-12 23:35:39 +0300

    before the carousel and collapsible nav dropdowns

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,29 @@ import { Component, ElementRef, Renderer2 } from '@angular/core';
 })
 export class AppComponent {
   title = 'company-site';
+  approach = 'health'
 
   constructor(private renderer: Renderer2) { }
 
-  changeAproachTxt(el: ElementRef) {
-    console.log(el)
+  changeAproachTxt(el: HTMLElement) {
+    console.log(el.id)
+    if (el.id === 'appr1') {
+      this.approach = 'health';
+    }
+    if (el.id === 'appr2') {
+      this.approach = 'markets'
+    }
+    if (el.id === 'appr3') {
+      this.approach = 'livelihood'
+    }
+    if (el.id === 'appr4') {
+      this.approach = 'environment'
+    }
+    if (el.id === 'appr5') {
+      this.approach = 'capacity'
+    }
+    if (el.id === 'appr6') {
+      this.approach = 'governance'
+    }
   }
 }
